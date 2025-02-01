1ba2d9a6bf4ca43ac06e4fd58f7ee66efdf40fea
matt-reya <mrw34@njit.edu>
2025-02-01 02:52:01 -0500

    feat: most of the code is done

---
+++ bathrooms.js
@@ -0,0 +1,181 @@
+window.bathrooms = {
+    "lsec3a": {
+        lat: 40.740632,
+        lon: -74.178454,
+        name: "Life Sciences and Engineering Center, Third Floor",
+        description: `Two bathrooms, located on the third floor of the Life Sciences and Engineering Center (LSEC).
+        
+        Once you enter the LSEC from the entrance closest to Summit Street, veer back right to the elevator/stairs, then head up to the third floor. The bathrooms are right by the elevator on the third floor.`,
+        img: 2
+    },
+    "colt1a": {
+        lat: 40.741411,
+        lon: -74.178005,
+        name: "Colton Hall, First Floor",
+        description: `One bathroom, located on the first floor/basement of Colton Hall (COLT).
+        
+        This bathroom isn't labeled "All Gender Single User Restroom" like the others, but it is single user and isn't intended for one specific gender. The sign on the door simply says "RESTROOM" and has symbols for both a woman and a man on it.
+        
+        Once you enter Colton Hall from the side closest to the Specht Building (where the bookstore is), head right down the stairs and then left down the little hallway until you get to the door at the end. The bathroom should be directly to your left at the end of the hallway.`,
+        img: 1
+    },
+    "colt2a": {
+        lat: 40.741323,
+        lon: -74.177561,
+        name: "Colton Hall Annex, Second Floor",
+        description: `One bathroom, located on the second floor of the Colton Hall (COLT) annex.
+        
+        The easiest way to get to this bathroom is via Weston Hall. Enter Weston Hall from the entrance closest to the Specht Building (where the bookstore is), head toward the hallway on the left, then make your first left into the area with the sign saying "NOW ENTERING COLTON" at the top:
+        <img src="img/colt2ainst1.jpg" alt="Entrance to the second floor Colton Hall annex from Weston Hall.">
+        Continue into Colton Hall and head towards the vending machine. The bathroom should be on the right. `,
+        img: 1
+    },
+    "colt3a": {
+        lat: 40.741323,
+        lon: -74.177561,
+        name: "Colton Hall Annex, Third Floor",
+        description: `One bathroom, located on the third floor of the Colton Hall (COLT) annex.
+        
+        The easiest way to get to this bathroom is via Weston Hall. Enter Weston Hall from the entrance closest to the Specht Building (where the bookstore is), and head up the elevator or stairs to the fourth floor of Weston Hall. Along the hallway is a room with a short staircase in it:
+        <img src="img/colt3ainst1.jpg" alt="Entrance to the third floor Colton Hall annex from Weston Hall.">
+        Head into that room, up the staircase, and through the door into Colton Hall. Then head around the corner and straight towards the exit staircase. The bathroom should be on your right.`,
+        img: 1
+    },
+    "camp5a": {
+        lat: 40.741580,
+        lon: -74.177506,
+        name: "Campbell Hall Annex, Fifth Floor",
+        description: `One bathroom, located in the Campbell Hall (CAMP) annex. This bathroom has been recorded as being on the fifth floor, due to it being right next to the CAMP 524 studio, but it's slightly unclear what floor it technically is on.
+        
+        This bathroom, while signed as an "All Gender Single User Restroom", is very clearly <em>not</em> a single user restroom, as it has stalls and even urinals. It is most likely a repurposed men's bathroom.
+        
+        The easiest way to get to this bathroom is via Weston Hall. Enter Weston Hall from the entrance closest to the Specht Building (where the bookstore is), and head up the elevator or stairs to the fifth floor of Weston Hall. Then walk down the hallway towards the faculty offices and make a left. Head through the two doors behind the pillar into the Campbell Hall annex:
+        <img src="img/camp5ainst1.jpg" alt="View of the corner of the fifth floor of Weston Hall.">
+        <img src="img/camp5ainst2.jpg" alt="Entrance to the fifth floor Campbell Hall annex from Weston Hall.">
+        Once you enter Campbell Hall, the bathroom should be on your right.`,
+        img: 1
+    },
+    "eber0a": {
+        lat: 40.742795,
+        lon: -74.177087,
+        name: "Eberhardt Hall, Basement",
+        description: `One bathroom, located in the basement of Eberhardt Hall (EBER).
+        
+        This bathroom isn't labeled "All Gender Single User Restroom" like the others, but it is single user and isn't intended for one specific gender. The sign next to the door simply says "Toilet" and has symbols for both a woman and a man on it.
+        
+        Once you enter Eberhardt Hall from the entrance closest to Cullimore Hall (you may need to tap your ID to do so), either take the elevator on the left down to the basement (floor B) or head up to floor 1, go down the left hallway, and take the stairs at the end down to the basement. Head down the basement hallway and the bathroom should be on your right.`,
+        img: 1
+    },
+    "ctr3a": {
+        lat: 40.742999,
+        lon: -74.177905,
+        name: "Campus Center, Third Floor",
+        description: `One bathroom, located on the third floor of the Campus Center (CTR).
+        
+        Once you enter the Campus Center from the entrance closest to Fenster Hall, head left and take the stairs or elevators up to floor 3. Then head down the hallway towards the Highlander Pub. The bathroom should be on the left, just after the two gendered bathrooms.`,
+        img: 1
+    },
+    "fmh1a": {
+        lat: 40.742047,
+        lon: -74.179019,
+        name: "Faculty Memorial Hall, First Floor, Front",
+        description: `One bathroom, located on the first floor of Faculty Memorial Hall (FMH).
+        
+        Once you enter Faculty Memorial Hall from the entrance closest to Kupfrian Hall, continue into the first floor and the bathroom should be right to your right.`,
+        img: 1
+    },
+    "fmh1b": {
+        lat: 40.741895,
+        lon: -74.179095,
+        name: "Faculty Memorial Hall, First Floor, Back",
+        description: `One bathroom, located on the first floor of Faculty Memorial Hall (FMH).
+        
+        Once you enter Faculty Memorial Hall from the entrance closest to Kupfrian Hall, continue into the first floor and head straight down the hallway. The bathroom should be to your right by the elevator just before you get to the corner.`,
+        img: 1
+    },
+    "fmh2a": {
+        lat: 40.742047,
+        lon: -74.179019,
+        name: "Faculty Memorial Hall, Second Floor",
+        description: `One bathroom, located on the second floor of Faculty Memorial Hall (FMH).
+        
+        Once you enter Faculty Memorial Hall from the entrance closest to Kupfrian Hall, head up the stairs to the second floor and the bathroom should be right to your right.`,
+        img: 1
+    },
+    "fmh3a": {
+        lat: 40.742047,
+        lon: -74.179019,
+        name: "Faculty Memorial Hall, Third Floor",
+        description: `One bathroom, located on the third floor of Faculty Memorial Hall (FMH).
+        
+        Once you enter Faculty Memorial Hall from the entrance closest to Kupfrian Hall, head up the stairs to the third floor and the bathroom should be right to your right.`,
+        img: 1
+    },
+    "fmh4a": {
+        lat: 40.742047,
+        lon: -74.179019,
+        name: "Faculty Memorial Hall, Fourth Floor",
+        description: `One bathroom, located on the fourth floor of Faculty Memorial Hall (FMH).
+        
+        This bathroom isn't labeled "All Gender Single User Restroom" like the others, but it is single user and isn't intended for one specific gender. The sign to the side of the door simply says "RESTROOM" and has symbols for both a woman and a man on it.
+        
+        Once you enter Faculty Memorial Hall from the entrance closest to Kupfrian Hall, head up the stairs to the fourth floor and the bathroom should be right to your right.`,
+        img: 1
+    },
+    "mhrh1a": {
+        lat: 40.741351,
+        lon: -74.180131,
+        name: "Martinson Honors Residence Hall, First Floor",
+        description: `One bathroom, located in the first floor lobby of the Martinson Honors Residence Hall (MHRH). Unlike the other residence halls, the first floor honors dorm lobby is open to anyone on campus during the day, however at night you do still need to be a resident and tap your ID to get in.
+        
+        Once you enter the honors dorm from the entrance closest to Warren Street, head to the back of the lobby and make a left towards the student kitchen. Walk towards the kitchen and the bathroom should be on your left.`,
+        img: 1
+    },
+    "tier1a": {
+        lat: 40.741916,
+        lon: -74.179641,
+        name: "Tiernan Hall, First Floor",
+        description: `One bathroom, located on the first floor of Tiernan Hall (TIER).
+        
+        Once you enter Tiernan Hall from the entrance closest to Kupfrian Hall, head right past the lecture hall and turn the corner at the end. The bathroom should be straight ahead at the end of the hallway (before turning the corner again).`,
+        img: 1
+    },
+    "tier3a": {
+        lat: 40.742255,
+        lon: -74.179550,
+        name: "Tiernan Hall, Third Floor",
+        description: `One bathroom, located on the third floor of Tiernan Hall (TIER).
+        
+        Once you enter Tiernan Hall from the entrance closest to Kupfrian Hall, head right towards the lecture hall and either take the elevator or the stairs at the corner of the hallway up to the third floor. The bathroom should be right next to the front elevator on the third floor.`,
+        img: 1
+    },
+    "wec2a": {
+        lat: 40.742128,
+        lon: -74.180577,
+        name: "Wellness and Events Center, Indoor Track (Second Floor)",
+        description: `One bathroom, located along the indoor track on the second floor of the Wellness and Events Center (WEC).
+        
+        This bathroom isn't labeled "All Gender Single User Restroom" like the others, but it is a family restroom and is labeled as such. However, this bathroom may be locked during the day and thus unable to be used.
+        
+        Once you enter the WEC from the entrance closest to Tiernan Hall, head up the stairs and then left towards the indoor track. You may need to tap your ID to get into the track, but once you do head straight along the track until you get to the enclave with the sign above it featuring a symbol of a woman. It should be just before the elevators. Head right into the enclave, then the family restroom should be on your right, just past the women's bathroom itself.`,
+        img: 1
+    },
+    "gitc2a": {
+        lat: 40.743994,
+        lon: -74.179039,
+        name: "Guttenberg Information Technologies Center, Makerspace II, Second Floor",
+        description: `Two bathrooms, located on the second floor of the Makerspace II building within the Guttenberg Information Technologies Center (GITC). In order to get to these bathrooms, you need to have access to the Makerspace, which can be obtained by completing the <a href="https://www.njitmakerspace.com/make-101-introduction-makerspace" target="_blank">MAKE 101 makerspace training</a>.
+
+        Once you enter the Makerspace II building from the entrance closest to the main GITC entrance, tap your physical ID card (you need the physical card, you cannot use the Symmetry app on your phone here) to get past the gate, and then head left towards the rooms with the glass windows. Make another left, go up the stairs to the mezzanine (where the 3D printers are), then go across the mezzanine towards the stairs in the back. Take those stairs up to the second floor, then walk down the short hallway. The bathrooms will be on your right.`,
+        img: 2
+    },
+    "mapl1a": {
+        lat: 40.741575,
+        lon: -74.182025,
+        name: "Maple Hall Building, First Floor",
+        description: `One bathroom, located on the first floor of the Maple Hall Building (MAPL). You need to be a resident of Maple Hall (or a guest of one) to get to this bathroom.
+        
+        Once you enter Maple Hall from the entrance closest to Warren Street, head around the front desk and walk straight down the hallway in the back past the study rooms towards the club room. The bathroom will be on your right.`,
+        img: 1
+    }
+}
